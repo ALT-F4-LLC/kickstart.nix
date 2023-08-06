af475448fe04c15b50fcca5eeb0f03761dbc1091
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-08-05 23:52:13 -0700

    feat(flake): abstract nix and home-manager configurations

---
+++ modules/configuration.nix
@@ -0,0 +1,23 @@
+{ username }:
+
+{
+  # add more nix-darwin settings here
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      builders-use-substitutes = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      trusted-users = [ "@wheel" ];
+      warn-dirty = false;
+    };
+  };
+  programs.zsh.enable = true;
+  services.nix-daemon.enable = true;
+  users.users.${username}.home = "/Users/${username}";
+}
