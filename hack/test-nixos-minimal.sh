d491e31ebdc1d93356de2c15ce5ea11c1210cb75
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-12-02 14:16:13 -0800

    fix(hack): update nixos-minimal test script

--- hack/test-nixos-minimal.sh
+++ hack/test-nixos-minimal.sh
@@ -9,6 +9,32 @@ cd "$dir"
 
 nix flake init -t "$root#nixos-minimal"
 
+sudo mkdir -p /etc/nixos
+
+sudo cat > /etc/nixos/hardware-configuration.nix <<EOF
+{ config, lib, pkgs, modulesPath, ... }: {
+  imports = [ ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "nvme" "usbhid" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  networking.useDHCP = lib.mkDefault true;
+  swapDevices = [ ];
+}
+EOF
+
+cat /etc/nixos/hardware-configuration.nix
+
 sed -i "s/<password>/password/g" flake.nix
 sed -i "s/<username>/user/g" flake.nix
 sed -i "s/throw //g" flake.nix
@@ -19,3 +45,5 @@ cat flake.nix
 echo "Initialized in $dir, proceeding with build step"
 
 nix build --impure --json .#nixosConfigurations.x86_64.config.system.build.toplevel
+
+rm -rf /etc/nixos
