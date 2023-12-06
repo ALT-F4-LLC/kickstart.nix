3619f635af829d314720f327c693cd8b3356848d
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-12-05 17:34:43 -0800

    fix(lib)

--- lib/default.nix
+++ lib/default.nix
@@ -8,15 +8,14 @@
       in
       pkgs.runCommand "darwin"
         {
-          buildInputs = with pkgs; [ coreutils ];
           src = ../template/darwin;
         } ''
         mkdir -p $out
-        $coreutils/bin/cp --no-preserve=mode -r $src/* $out
+        cp --no-preserve=mode -r $src/* $out
         sed -i -e "s/<username>/username/g" $out/flake.nix
         sed -i -e "s/throw //g" $out/flake.nix
         sed -i -e "s/ # TODO.*$//g" $out/flake.nix
-        $coreutils/bin/cat $out/flake.nix
+        cat $out/flake.nix
       '';
 
     nixos-desktop = system: desktop:
