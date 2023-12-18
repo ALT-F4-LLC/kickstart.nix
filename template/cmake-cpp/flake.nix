a106dd99763e43cc1758628da90aa6711dc626fb
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-12-18 13:54:01 +0000

    feat: init c++ cmake example

---
+++ template/cmake-cpp/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Example kickstart C/C++ cmake project.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          inherit (pkgs) dockerTools cmake just;
+          inherit (dockerTools) buildImage;
+          name = "example";
+          version = "0.1.0";
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self'.packages.default ];
+          };
+
+          packages = {
+            default = pkgs.stdenv.mkDerivation {
+              inherit version;
+              pname = name;
+              src = ./.;
+
+              buildInputs = [ cmake ];
+
+              configurePhase = ''
+                cmake .
+              '';
+
+              buildPhase = ''
+                make
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                mv cmake_example $out/bin/${name}
+              '';
+            };
+
+            docker = buildImage {
+              inherit name;
+              tag = version;
+              config = {
+                Cmd = "${self'.packages.default}/bin/${name}";
+              };
+            };
+          };
+        };
+    };
+}
