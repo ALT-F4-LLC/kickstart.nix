41915e347368049b2837988695a146f312e94fc1
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-12-22 15:13:38 -0800

    feat(template): added basic lua-app

---
+++ template/lua-app/src/example.lua
@@ -0,0 +1,3 @@
+local example = require "./example_lib"
+
+example.hello_world()
---
+++ template/lua-app/src/example_lib.lua
@@ -0,0 +1,7 @@
+local function hello_world()
+    print("Hello World!")
+end
+
+return {
+    hello_world = hello_world
+}
