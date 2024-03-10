45797266a5f6c96108e1468f6c752435c6fafdd6
Hayden <22327045+hbjydev@users.noreply.github.com>
2024-03-10 16:25:42 +0000

    Vite React Template (#57)
    * feat(vite): init template from create-vite-app source
    
    * feat(templates): add vite-react template to flake, lib, CI and README

---
+++ template/vite-react/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Example kickstart Node.js backend project.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        name = "example";
+        version = "0.1.0";
+        nodejs = pkgs.nodejs_21;
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [pkgs.prefetch-npm-deps];
+            inputsFrom = [self'.packages.default];
+          };
+        };
+
+        packages = {
+          default = pkgs.buildNpmPackage {
+            inherit version nodejs;
+            pname = name;
+            src = ./.;
+            npmDepsHash = "sha256-KeXRIp4qNywb1sy5lXTagoUsW6EeK1kF5OWJ97w9Vfk=";
+            installPhase = ''
+              cp --no-preserve=mode -r dist $out
+            '';
+          };
+        };
+      };
+    };
+}
