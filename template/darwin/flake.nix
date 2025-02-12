5fb9591084b55c427a94b6ee89ba064a0260b267
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-02-12 11:14:07 -0800

    chore: update home-manager and nixpkgs to release-25.05
    Updated the home-manager and nixpkgs URLs in the flake.nix file to point to the release-25.05 versions. This ensures compatibility with the latest features and improvements in the new release.

--- template/darwin/flake.nix
+++ template/darwin/flake.nix
@@ -5,8 +5,8 @@
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = "github:lnl7/nix-darwin";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = inputs @ {
