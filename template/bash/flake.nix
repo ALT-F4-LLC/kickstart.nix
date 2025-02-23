9bc2bd8960c138639a65d6f9ca5da1b4fd5b6150
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-23 10:36:38 -0500

    fix(template): explicitly declare flake-parts input
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- template/bash/flake.nix
+++ template/bash/flake.nix
@@ -1,7 +1,10 @@
 {
   description = "Example kickstart Bash application project.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
